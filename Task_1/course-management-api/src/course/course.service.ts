2020e7eacaa553573123b7e781f8837d7dcc10b9
Mahmodul Hasan Ismail <mahmodulhasan8091@gmail.com>
2026-07-12 03:24:22 +0600

    Implement GET endpoints for Course module

--- Task_1/course-management-api/src/course/course.service.ts
+++ Task_1/course-management-api/src/course/course.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    getAllCourses(): string {
+        return `Get all courses - from service`
+    }
+
+    getCourseById(id: string): string {
+        return `Get all course with id : ${id} from service`
+    }
+}
+
